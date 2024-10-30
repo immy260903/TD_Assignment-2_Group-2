a012a21b8bad32a58a5e301bd7fa8120b3dd6d19
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-10-30 19:49:37 +1100

    tool 3 added UI

--- scripts/tool3.py
+++ scripts/tool3.py
@@ -19,56 +19,78 @@ if the stored file name is different than the referenced file name:
 reload all referenced assets
 '''
 
-workspace_path = cmds.workspace(q=True, rd=True)
+def find_and_replace_new_reference_versions(*args):
+	workspace_path = cmds.workspace(q=True, rd=True)
+	log_text = "Scanning references at " + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-# Get all references in the scene
-references = cmds.file(query=True, reference=True) 
-updated_references = []
+	# Get all references in the scene
+	references = cmds.file(query=True, reference=True) 
+	updated_references = []
 
-for ref in references:
+	for ref in references:
 
-	print(ref)
+		print(ref)
 
-	# Get file path and version number of maya reference
-	ref_path = cmds.referenceQuery(ref, filename=True)
-	print(ref_path)
+		# Get file path and version number of maya reference
+		ref_path = cmds.referenceQuery(ref, filename=True)
+		print(ref_path)
 
-	relative_dir = os.path.relpath(os.path.dirname(ref_path), workspace_path)
-	print("Scanning directory: " + relative_dir)
-	maya_name = os.path.basename(ref_path)
-	maya_ver = re.search(r'\.v(\d+)\.', maya_name).group(1)
+		relative_dir = os.path.relpath(os.path.dirname(ref_path), workspace_path)
+		print("Scanning directory: " + relative_dir)
+		maya_name = os.path.basename(ref_path)
+		maya_ver = re.search(r'\.v(\d+)\.', maya_name).group(1)
 
-	# Compare version number in maya to version numbers on disk
-	new_ver = maya_ver
-	new_path = ""
-	absolute_dir = os.path.join(workspace_path, relative_dir)
-	for root, dirs, files in os.walk(absolute_dir):
-		for file in files:
-			match = re.search(r'\.v(\d+)\.', os.path.basename(file))
-			if match:
-				ver = match.group(1)
-				if int(maya_ver) < int(ver):
-					new_ver = ver
-					new_path = file
-		break
-	
-	# If a new version was found, add it to the updated references
-	if new_path != "":
-		print("Found new version: " + str(new_path))
+		# Compare version number in maya to version numbers on disk
+		new_ver = maya_ver
+		new_path = ""
+		absolute_dir = os.path.join(workspace_path, relative_dir)
+		for root, dirs, files in os.walk(absolute_dir):
+			for file in files:
+				match = re.search(r'\.v(\d+)\.', os.path.basename(file))
+				if match:
+					ver = match.group(1)
+					if int(maya_ver) < int(ver):
+						new_ver = ver
+						new_path = file
+			break
+		
+		# If a new version was found, add it to the updated references
+		if new_path != "":
+			print("Found new version: " + str(new_path))
 
-		ref_node = cmds.referenceQuery(ref, referenceNode=True)
-		updated_references.append((ref_node, new_path))
+			ref_node = cmds.referenceQuery(ref, referenceNode=True)
+			updated_references.append((ref_node, new_path))
+		else:
+			print("No new version found.")
+
+	# If there are newer versions, replace the references
+	if updated_references:
+		print("Replacing references...")
+		for ref_node, new_path in updated_references:
+			try:
+				cmds.file(new_path, loadReference = ref_node)
+				print(f"Replaced {new_path}")
+				log_text += f"\nUpdated reference {new_path}"
+			except Exception as e:
+				print(f"Error replacing {new_path}: {e}")
+				log_text += f"\nError updating reference {new_path}: {e}"
 	else:
-		print("No new version found.")
+		print("All references are up-to-date.")
+		log_text += "\nAll references are up-to-date."
+	
+	cmds.text("log", edit=True, label=log_text)
 
-# If there are newer versions, replace the references
-if updated_references:
-	print("Replacing references...")
-	for ref_node, new_path in updated_references:
-		try:
-			cmds.file(new_path, loadReference = ref_node)
-			print(f"Replaced {new_path}")
-		except Exception as e:
-			print(f"Error replacing {new_path}: {e}")
-else:
-	print("All references are up-to-date.")
+if cmds.window('referenceUpdater', exists = True):
+    cmds.deleteUI('referenceUpdater')
+
+cmds.window('referenceUpdater', title="Reference Updater", resizeToFitChildren=True, sizeable=False)
+
+columnMain = cmds.columnLayout(adjustableColumn=True, columnAlign="center", rowSpacing=10)
+
+frame = cmds.frameLayout(label="LOG", collapsable=True, borderVisible=True, marginHeight=10, marginWidth=10, bgc=[0.05, 0.36, 0.36])
+
+cmds.button(label = 'Scan and Update References', command = 'find_and_replace_new_reference_versions()', bgc=[0.0, 0.5, 0.5])
+cmds.text("log", label="")
+
+# Show
+cmds.showWindow('referenceUpdater')
